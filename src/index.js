6836cdc4ebbd2692299d7686a44b363831cbcae9
Ben Nortier <ben@bjnortier.com>
2018-09-07 16:59:17 +0200

    index

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import Dialog, { Header, Footer } from './Dialog'
+import HSpace from './HSpace'
+import IconButton from './IconButton'
+import Input from './Input'
+import RefreshButton from './RefreshButton'
+import reset from './reset'
+import Spinner from './Spinner'
+import Switch from './Switch'
+import TextButton from './TextButton'
+import Verified from './Verified'
+import VSpace from './VSpace'
+
+export {
+  reset,
+  Spinner, Verified,
+  Dialog, Header, Footer,
+  HSpace, VSpace,
+  IconButton, RefreshButton, TextButton,
+  Input, Switch
+}
