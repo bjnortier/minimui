82e4e6b70db99fd3f606eb9c915727464a18d750
Ben Nortier <ben@bjnortier.com>
2019-01-30 17:32:33 +0200

    fix standardjs

--- test/functional/forms.test.js
+++ test/functional/forms.test.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { faClipboard, faTrashAlt } from '@fortawesome/free-regular-svg-icons'
 import { faFilter } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styled from 'styled-components'
 
 import {
   HSpace,
