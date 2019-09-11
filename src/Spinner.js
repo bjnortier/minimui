8ec23e8921bbe3fdc0ea91208df87cc9fbf44345
Ben Nortier <ben@bjnortier.com>
2019-09-11 15:20:04 +0200

    Spinner is CSS no SVG

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,37 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 const Pad = styled.span`
   padding-left: ${({ padLeft }) => padLeft ? 4 : 0}px;
 `
 
-const Icon = styled.div`
-  display: inline-block;
-  width: 16px;
-  height: 16px;
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 `
 
-const svg = `
-<!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
-<svg width="100%" height="100%" viewBox="-2 -2 37 37" xmlns="http://www.w3.org/2000/svg">
-    <g stroke="#666" fill="none" fill-rule="evenodd">
-        <g transform="translate(1 1)" stroke-width="4">
-            <circle stroke-opacity=".25" cx="16" cy="16" r="16" />
-            <path d="M 16 0 A 16 16 0 0 0 0 16">
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                from="0 16 16"
-                to="360 16 16"
-                dur="1s"
-                repeatCount="indefinite"
-              />
-            </path>
-        </g>
-    </g>
-</svg>
+const Icon = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 16px;
+  height: 16px;
+  border: 2px solid #66666644;
+  border-top: 2px solid #666;
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
 `
 
 export default (props) => <Pad padLeft={props.padLeft}>
-  <Icon dangerouslySetInnerHTML={{ __html: svg }} />
+  <Icon />
 </Pad>
